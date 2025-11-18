f79f192f69ed6a901738a639fa26ca4673b9fa0e
Andrei Statescu <27307694+AndreiRStatescu@users.noreply.github.com>
2025-11-18 08:09:29 +0200

    Add templates + add convex base installation

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
 
 const Home = () => {
   return (
@@ -9,11 +8,7 @@ const Home = () => {
         <Navbar />
       </div>
       <div className="mt-16">
-        Click{" "}
-        <Link href="/documents/123">
-          <span className="text-blue-500 underline">here</span>
-        </Link>{" "}
-        to go to Documents 123 page.
+        <TemplatesGallery />
       </div>
     </div>
   );
---
+++ src/app/constants/templates.ts
@@ -0,0 +1,13 @@
+export const templates = [
+  { id: "blank", label: "Blank Document", imgUrl: "/templates/blank-document.svg" },
+  { id: "business-letter", label: "Business Letter", imgUrl: "/templates/business-letter.svg" },
+  { id: "cover-letter", label: "Cover Letter", imgUrl: "/templates/cover-letter.svg" },
+  { id: "letter", label: "Letter", imgUrl: "/templates/letter.svg" },
+  { id: "project-proposal", label: "Project Proposal", imgUrl: "/templates/project-proposal.svg" },
+  { id: "resume", label: "Resume", imgUrl: "/templates/resume.svg" },
+  {
+    id: "software-proposal",
+    label: "Software Proposal",
+    imgUrl: "/templates/software-proposal.svg",
+  },
+];
