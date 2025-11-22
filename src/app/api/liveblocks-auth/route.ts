41410b011553a091c5428e1d6d8990f084fafc95
Andrei Statescu <27307694+AndreiRStatescu@users.noreply.github.com>
2025-11-22 09:01:51 +0200

    Update route.ts

---
+++ src/app/api/liveblocks-auth/route.ts
@@ -0,0 +1,20 @@
+import { Liveblocks } from "@liveblocks/node";
+import { ConvexHttpClient} from "convex/browser";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export async function POST(request: Request) {
+  const {sessionClaims} = await auth();
+  if (!sessionClaims) {
+    return new Response("Unauthorized", {status: 401});
+  }
+
+  const user = await currentUser();
+  if (!user) {
+    return new Response("Unauthorized", {status: 401});
+  }
+
+  const { room } = await request.json();
+
+}
