cb926f5536f192f2ed25cafc2c317977fb87d7a7
Andrei Statescu <27307694+AndreiRStatescu@users.noreply.github.com>
2025-11-24 11:57:52 +0200

    More documents per page

--- src/app/constants/defaults.ts
+++ src/app/constants/defaults.ts
@@ -1,5 +1,5 @@
 export const AVATAR_SIZE = 36;
-export const HOME_DOCUMENTS_PER_PAGE = 5;
+export const HOME_DOCUMENTS_PER_PAGE = 10;
 
 export const DOC_INITIAL_LEFT_MARGIN = 56;
 export const DOC_INITIAL_RIGHT_MARGIN = 56;
